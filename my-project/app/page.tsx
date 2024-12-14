0952a5d96430b2e38075a43dd6aed520fef92840
AbulQasimbintariq <www.qasimbhalli6547@gmail.com>
2024-12-14 21:54:27 +0500

    add pics

---
+++ my-project/app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./navbar"
+import "./globals.css";
+export default function Home() {
+  return (
+          <div className="Home">
+          <Navbar />
+            <h1>Hello World</h1>
+            </div>
+
+  );
+}
